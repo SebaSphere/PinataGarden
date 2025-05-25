cda7ddea203fc2bdb805224196d319e4bf731b74
SebaSphere <SebaSphereEmails@gmail.com>
2025-05-25 15:40:59 -0400

    chore: initial commit

---
+++ src/client/java/dev/sebastianb/pinatagarden/client/PinataGardenClient.java
@@ -0,0 +1,10 @@
+package dev.sebastianb.pinatagarden.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PinataGardenClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/dev/sebastianb/pinatagarden/PinataGarden.java
@@ -0,0 +1,10 @@
+package dev.sebastianb.pinatagarden;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PinataGarden implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
