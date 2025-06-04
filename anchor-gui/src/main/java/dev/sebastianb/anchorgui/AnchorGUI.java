f20983f93040dd4d1f679104e106e9495b3b7e03
sebasphere <sebasphereemails@gmail.com>
2025-06-04 00:25:30 -0400

    chore: start of a new GUI lib

---
+++ anchor-gui/src/main/java/dev/sebastianb/anchorgui/AnchorGUI.java
@@ -0,0 +1,12 @@
+package dev.sebastianb.anchorgui;
+
+import net.fabricmc.api.ModInitializer;
+
+public class AnchorGUI implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        System.out.println("Anchoring the GUIs");
+    }
+
+}
